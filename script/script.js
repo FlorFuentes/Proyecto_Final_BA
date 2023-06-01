6fc83db5c9b4636a596f8f31d9d92e2abbf88a86
FlorFuentes <flopy_200412@hotmail.com>
2023-06-01 17:08:27 -0300

    Formulario terminado

--- script/script.js
+++ script/script.js
@@ -1,21 +1,16 @@
-/* // Llamada a la API del clima
-fetch('URL_DE_LA_API&APPID=TU_CLAVE_DE_API')
-  .then(response => response.json())
-  .then(data => {
-    // Obtener la información relevante del clima
-    const weatherCondition = data.weather[0].main;
+const abrirFormulario = document.getElementById('abrirFormulario');
 
-    // Obtener el elemento de imagen en tu página
-    const weatherImage = document.getElementById('weather-image');
+const formulario = document.getElementById('contForm');
 
-    // Actualizar la imagen en función de la condición climática
-    if (weatherCondition === 'Rain' || weatherCondition === 'Thunderstorm') {
-      weatherImage.src = 'ruta_al_logo_cafe_caliente.png';
-    } else if (weatherCondition === 'Clear') {
-      weatherImage.src = 'ruta_al_jugo_naranja.png';
-    }
-  })
-  .catch(error => {
-    console.log('Error al obtener datos del clima:', error);
-  });
- */
+abrirFormulario.addEventListener('click', function (event) {
+  event.preventDefault();
+  // Vemos si el formulario está visible o oculto
+  const formularioVisible = formulario.style.display !== 'none';
+
+  // Cambiamos el estado del formulario (visible u oculto) según su estado actual
+  if (formularioVisible) {
+    formulario.style.display = 'none';
+  } else {
+    formulario.style.display = 'block';
+  }
+});
